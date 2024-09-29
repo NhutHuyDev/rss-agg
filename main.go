05525ad91fe2dd2ba38b97ed7a00bfeeaf60e259
huynguyendev <nguyennhuthuy.dev@.com>
2024-09-29 19:55:54 +0700

    ADD: database config

--- main.go
+++ main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/NhutHuyDev/rss-agg/internal/db"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *db.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
 
-	port := os.Getenv("PORT")
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbConnectioStr := os.Getenv("DB_CONNECTION_STR")
+	if dbConnectioStr == "" {
+		log.Fatal("DB_CONNECTION_STR is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbConnectioStr)
+	if err != nil {
+		log.Fatal("Cannot connection to database")
+	}
+
+	apiCfg := apiConfig{
+		DB: db.New(conn),
+	}
 
 	router := chi.NewRouter()
 
@@ -28,19 +52,19 @@ func main() {
 
 	// Routes
 	v1Router := chi.NewRouter()
-	v1Router.HandleFunc("/healthz", HandlerReadiness)
-	v1Router.HandleFunc("/notfound", HandlerErr)
+	v1Router.Get("/healthz", HandlerReadiness)
+	v1Router.Post("/users", apiCfg.HandlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
 	server := &http.Server{
-		Addr:    ":" + port,
+		Addr:    ":" + portStr,
 		Handler: router,
 	}
 
-	log.Printf("Server is starting on port %v", port)
-	// listenAndServer() is a blocking function
-	err := server.ListenAndServe()
+	log.Printf("Server is starting on port %v", portStr)
+	// Note: listenAndServer() is a blocking function
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
