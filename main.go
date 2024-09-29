e08833732d9b43c242f20f628faaffc6e37c4641
huynguyendev <nguyennhuthuy.dev@.com>
2024-09-29 14:19:21 +0700

    ADD: boilerplace for http server

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello from rss-agg")
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	// Routes
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", HandlerReadiness)
+	v1Router.HandleFunc("/notfound", HandlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	log.Printf("Server is starting on port %v", port)
+	// listenAndServer() is a blocking function
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
